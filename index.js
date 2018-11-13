789438ee14fc528c8708eb0a8ee3bdfb8b3b7cec
Josh Grant <josh@synergitech.co.uk>
2018-11-13 11:24:22 +0000

    code style

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 const puppeteer = require('puppeteer');
 const allowedFormats = [
-    "Letter",
-    "Legal",
-    "Tabloid",
-    "Ledger",
-    "A0",
-    "A1",
-    "A2",
-    "A3",
-    "A4",
-    "A5",
-    "A6",
+    'Letter',
+    'Legal',
+    'Tabloid',
+    'Ledger',
+    'A0',
+    'A1',
+    'A2',
+    'A3',
+    'A4',
+    'A5',
+    'A6',
 ];
 const resolve = require('path').resolve;
-const fs = require('fs');
+const readFileSync = require('fs').readFileSync;
 
 var args = require('yargs')
     .usage('$0 <command> [args]')
@@ -51,34 +51,34 @@ var args = require('yargs')
         describe: 'Specify viewport options, e.g., width=1920,height=1080,isMobile=true',
         requiresArg: true,
         coerce: (arg) => {
-            var opts = arg.split(",");
+            var opts = arg.split(',');
             var viewport = {};
 
             for (var i in opts) {
                 var line = opts[i];
-                var setting = line.split("=");
+                var setting = line.split('=');
                 if (setting.length != 2) {
-                    throw "Incorrect parameter format for `viewport`";
+                    throw 'Incorrect parameter format for `viewport`';
                 }
                 var key = setting[0];
                 var val = setting[1];
-                if (key == "width") {
+                if (key == 'width') {
                     viewport.width = parseInt(val);
                 }
-                if (key == "height") {
+                if (key == 'height') {
                     viewport.height = parseInt(val);
                 }
-                if (key == "deviceScaleFactor") {
+                if (key == 'deviceScaleFactor') {
                     viewport.deviceScaleFactor = parseFloat(val);
                 }
-                if (key == "isMobile") {
-                    viewport.isMobile = val == "true";
+                if (key == 'isMobile') {
+                    viewport.isMobile = val == 'true';
                 }
-                if (key == "hasTouch") {
-                    viewport.hasTouch = val == "true";
+                if (key == 'hasTouch') {
+                    viewport.hasTouch = val == 'true';
                 }
-                if (key == "isLandscape") {
-                    viewport.isLandscape = val == "true";
+                if (key == 'isLandscape') {
+                    viewport.isLandscape = val == 'true';
                 }
             }
 
@@ -87,79 +87,79 @@ var args = require('yargs')
     })
     .option('debug', {
         default: false,
-        describe: "Run Chrome with a head and keep the browser open after render",
+        describe: 'Run Chrome with a head and keep the browser open after render',
         boolean: true,
     })
     .command('pdf', 'Output content rendered as PDF', function (args) {
         return args
             .option('landscape', {
                 default: false,
-                describe: "Page orientation",
+                describe: 'Page orientation',
                 boolean: true,
             })
             .option('scale', {
                 default: 1,
-                describe: "Scale of the webpage rendering",
+                describe: 'Scale of the webpage rendering',
                 requiresArg: true,
             })
             .option('displayHeaderFooter', {
                 default: false,
-                describe: "Display header and footer",
+                describe: 'Display header and footer',
                 boolean: true,
             })
             .option('headerTemplate', {
-                describe: "HTML template for the print header",
+                describe: 'HTML template for the print header',
                 requiresArg: true,
                 coerce: (val) => {
                     try {
-                        return fs.readFileSync(val).toString();
+                        return readFileSync(val).toString();
                     } catch (err) {
                         return val;
                     }
                 }
             })
             .option('footerTemplate ', {
-                describe: "HTML template for the print footer",
+                describe: 'HTML template for the print footer',
                 requiresArg: true,
                 coerce: (val) => {
                     try {
-                        return fs.readFileSync(val).toString();
+                        return readFileSync(val).toString();
                     } catch (err) {
                         return val;
                     }
                 }
             })
             .option('printBackground', {
-                describe: "Print background graphics",
+                describe: 'Print background graphics',
                 boolean: true,
                 default: false,
             })
             .option('pageRanges', {
-                describe: "Paper ranges to print, e.g., '1-5, 8, 11-13'",
+                describe: 'Paper ranges to print, e.g., \'1-5, 8, 11-13\'',
                 default: '',
                 requiresArg: true,
                 type: 'string',
             })
             .option('format', {
-                describe: "Paper format. If set, takes priority over width or height options",
+                describe: 'Paper format. If set, takes priority over width or height options',
                 default: 'Letter',
                 requiresArg: true,
             })
             .option('width', {
-                describe: "Paper width, accepts values labeled with units",
+                describe: 'Paper width, accepts values labeled with units',
                 requiresArg: true,
             })
             .option('height', {
-                describe: "Paper height, accepts values labeled with units",
+                describe: 'Paper height, accepts values labeled with units',
                 requiresArg: true,
             })
             .option('margin', {
-                describe: "Paper margins, defaults to none, top,right,bottom,left e.g., 20px,20px,20px,20px",
+                describe: 'Paper margins, defaults to none, top,right,bottom,left e.g., 20px,20px,20px,20px',
                 requiresArg: true,
                 coerce: (arg) => {
-                    var split = arg.split(",");
+                    var split = arg.split(',');
                     if (split.length != 4) {
-                        throw "Wrong number of margins specified";
+                        throw 'Wrong number of margins specified';
                     }
 
                     return {
@@ -167,65 +167,65 @@ var args = require('yargs')
                         right: split[1],
                         bottom: split[2],
                         left: split[3]
-                    }
+                    };
                 }
             })
             .option('preferCSSPageSize', {
-                describe: "Give any CSS @page size declared in the page priority over what is declared in width and height or format options",
+                describe: 'Give any CSS @page size declared in the page priority over what is declared in width and height or format options',
                 boolean: true,
                 default: false
             });
     })
     .command('screenshot', 'Output content rendered as an image', function (args) {
         return args
-        .option('type', {
-            default: 'png',
-            describe: "Specify screenshot type",
-            choices: ['jpeg', 'png'],
-        })
-        .option('quality', {
-            describe: "The quality of the image, between 0-100. Not applicable to png images",
-            requiresArg: true,
-        })
-        .option('fullPage', {
-            describe: "When set, takes a screenshot of the full scrollable page",
-            boolean: true,
-            default: false,
-        })
-        .option('clip', {
-            describe: "Specify clipping region of the page, e.g., x,y,width,height",
-            requiresArg: true,
-            coerce: (arg) => {
-                var split = arg.split(",");
-                if (split.length != 4) {
-                    throw "Wrong number of clipping region args specified";
-                }
+            .option('type', {
+                default: 'png',
+                describe: 'Specify screenshot type',
+                choices: ['jpeg', 'png'],
+            })
+            .option('quality', {
+                describe: 'The quality of the image, between 0-100. Not applicable to png images',
+                requiresArg: true,
+            })
+            .option('fullPage', {
+                describe: 'When set, takes a screenshot of the full scrollable page',
+                boolean: true,
+                default: false,
+            })
+            .option('clip', {
+                describe: 'Specify clipping region of the page, e.g., x,y,width,height',
+                requiresArg: true,
+                coerce: (arg) => {
+                    var split = arg.split(',');
+                    if (split.length != 4) {
+                        throw 'Wrong number of clipping region args specified';
+                    }
 
-                return {
-                    x: split[0],
-                    y: split[1],
-                    width: split[2],
-                    height: split[3]
+                    return {
+                        x: split[0],
+                        y: split[1],
+                        width: split[2],
+                        height: split[3]
+                    };
                 }
-            }
-        })
-        .option('omitBackground', {
-            describe: "Hides default white background and allows capturing screenshots with transparency",
-            boolean: true,
-            default: false,
-        })
-        .option('encoding', {
-            describe: "The encoding of the image",
-            choices: ["base64", "binary"],
-            default: "binary",
-            requiresArg: true,
-        })
+            })
+            .option('omitBackground', {
+                describe: 'Hides default white background and allows capturing screenshots with transparency',
+                boolean: true,
+                default: false,
+            })
+            .option('encoding', {
+                describe: 'The encoding of the image',
+                choices: ['base64', 'binary'],
+                default: 'binary',
+                requiresArg: true,
+            });
     })
     .demandCommand(1, 1)
-    .check(function (argv, opts) {
+    .check(function(argv, opts) {
         var cmd = argv._[0];
-        if (cmd != "pdf" && cmd != "screenshot") {
-            throw "Unknown command: "+cmd;
+        if (cmd != 'pdf' && cmd != 'screenshot') {
+            throw 'Unknown command: ' + cmd;
         }
 
         if (argv.scale < 0.1 || argv.scale > 2) {
@@ -251,7 +251,7 @@ if (args.debug) {
     if (cmd != 'pdf') {
         debug = true;
     } else {
-        console.warn("Debug mode incompatible with PDF rendering");
+        console.warn('Debug mode incompatible with PDF rendering');
     }
 }
 
@@ -322,14 +322,13 @@ var generateScreenshotConfig = (args) => {
         if (args.page) {
             location = args.page;
         } else if (args.content) {
-            location = "data:text/html," + args.content;
+            location = 'data:text/html,' + args.content;
         } else if (args.file) {
-            filePath = resolve(args.file);
-            location = 'file:///' + filePath;
+            location = 'file:///' + resolve(args.file);
         } else {
             var input = await (async function() {
                 var prom = new Promise((resolve) => {
-                    var input = "";
+                    var input = '';
 
                     process.stdin.setEncoding('utf8');
                     process.stdin.on('readable', async () => {
@@ -346,10 +345,12 @@ var generateScreenshotConfig = (args) => {
 
                 return prom;
             })();
-            location = "data:text/html," + input;
+            location = 'data:text/html,' + input;
         }
 
-        await page.goto(location, {waitUntil: args.waitUntil});
+        await page.goto(location, {
+            waitUntil: args.waitUntil
+        });
         page.emulateMedia(args.emulateMedia);
 
         var output = false;
@@ -360,13 +361,13 @@ var generateScreenshotConfig = (args) => {
             output = await page.screenshot(generateScreenshotConfig(args));
         }
         if (output === false) {
-            console.error("Unable to generate output");
+            console.error('Unable to generate output');
             process.exit(1);
         }
 
         if (args.path) {
             if (process.stdout.isTTY) {
-                console.log("Output saved to "+args.path);
+                console.log('Output saved to ' + args.path);
             }
         } else {
             if (process.stdout.isTTY) {
